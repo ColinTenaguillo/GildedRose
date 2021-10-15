5a66a01e45d4f370ffcaa6251cec32f032e5c79e
Nicolas Roche <nicolasr@itaf.tech>
2021-10-15 15:58:54 +0200

    Basic use-cases all tests Ok

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,75 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const {
+  Shop,
+  Item
+} = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
+describe("Gilded Rose", function () {
+  it("should foo", function () {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
   });
+
+  it("Aged Brie sellIn 2 & quality 0 should return sellIn 1 & quality 1 ", () => {
+    const gilded = new Shop([new Item("Aged Brie", 2, 0)])
+    const items = gilded.updateQuality();
+    expect(items[0].sellIn).toBe(1);
+    expect(items[0].quality).toBe(1);
+  })
+
+  it("An item can never be more than 50 ", () => {
+    const gilded = new Shop([new Item("Aged Brie", 1, 50)])
+    const items = gilded.updateQuality();
+    expect(items[0].sellIn).toBe(0);
+    expect(items[0].quality).toBe(50);
+  })
+
+  it("Normal items should decrease 1 quality & 1 sellIn", () => {
+    const gilded = new Shop([new Item("NormalItem", 10, 20)])
+    const items = gilded.updateQuality();
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(19);
+  })
+
+  it("Items quality should never be below 0", () => {
+    const gilded = new Shop([new Item("NomalItem", 10, 0)])
+    const items = gilded.updateQuality();
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(0);
+  })
+
+  it("Item sellIn at 0 should decrease quality by two", () => {
+    const gilded = new Shop([new Item("NomalItem", 0, 10)])
+    const items = gilded.updateQuality();
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(8);
+  })
+
+  it("legendaray item never loose quality", () => {
+    const gilded = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 80)])
+    const items = gilded.updateQuality();
+    expect(items[0].sellIn).toBe(0);
+    expect(items[0].quality).toBe(80);
+  })
+
+  it("Backstage passes sellIn <= 10 quality increase by two", () => {
+    const gilded = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 8, 20)])
+    const items = gilded.updateQuality();
+    expect(items[0].sellIn).toBe(7);
+    expect(items[0].quality).toBe(22);
+  })
+
+  it("Backstage passes sellIn <= 5 quality increase by three", () => {
+    const gilded = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 4, 20)])
+    const items = gilded.updateQuality();
+    expect(items[0].sellIn).toBe(3);
+    expect(items[0].quality).toBe(23);
+  })
+
+  it("Backstage passes sellIn 0 quality is 0", () => {
+    const gilded = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)])
+    const items = gilded.updateQuality();
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(0);
+  })
 });
